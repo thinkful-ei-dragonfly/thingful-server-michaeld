65bfe1d04bfe26c0fb2f1c49de3a481315535285
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-06-28 12:32:58 -0400

    added jwt authentication

--- src/middleware/basic-auth.js
+++ src/middleware/basic-auth.js
@@ -1,36 +1,31 @@
-const bcrypt = require('bcryptjs')
-const knex = require('knex');
+const AuthService = require('../auth/auth-service')
 
-function requireAuth(req,res,next) {
- 
-  const authToken = req.get('Authorization') || '';
+function requireAuth(req, res, next) {
+  const authToken = req.get('Authorization') || ''
 
-  if(!authToken.toLowerCase().startsWith('basic ')){
-    return res.status(401).json({error: 'Missing token'});
+  let basicToken
+  if (!authToken.toLowerCase().startsWith('basic ')) {
+    return res.status(401).json({ error: 'Missing basic token' })
   } else {
     basicToken = authToken.slice('basic '.length, authToken.length)
   }
 
-  const [_, token] = authToken.split(' ');
+  const [tokenUserName, tokenPassword] = AuthService.parseBasicToken(basicToken)
 
-  const [tokenUserName, tokenPassword] = Buffer
-     .from(basicToken, 'base64')
-     .toString()
-     .split(':')
+  if (!tokenUserName || !tokenPassword) {
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
 
-
-     if (!tokenUserName || !tokenPassword) {
+  AuthService.getUserWithUserName(
+    req.app.get('db'),
+    tokenUserName
+  )
+    .then(user => {
+      if (!user) {
         return res.status(401).json({ error: 'Unauthorized request' })
       }
 
-  req.app.get('db')('thingful_users')
-    .where({user_name: tokenUserName})
-    .first()
-    .then(user =>{
-        if (!user) {
-        return res.status(401).json({error:'Unauthorized request'});
-      }
-      return bcrypt.compare(tokenPassword, user.password)
+      return AuthService.comparePasswords(tokenPassword, user.password)
         .then(passwordsMatch => {
           if (!passwordsMatch) {
             return res.status(401).json({ error: 'Unauthorized request' })
@@ -38,12 +33,11 @@ function requireAuth(req,res,next) {
 
           req.user = user
           next()
-      })
+        })
     })
-    .catch(next);
-
+    .catch(next)
 }
 
 module.exports = {
   requireAuth,
-};
+}
--- test/protected-endpoints.spec.js
+++ test/protected-endpoints.spec.js
@@ -1,6 +1,7 @@
 const knex = require('knex');
 const app = require('../src/app');
 const helpers = require('./test-helpers');
+const config = require('../src/config');
 
 describe('Protected Endpoints', function() {
   let db;
@@ -21,9 +22,9 @@ describe('Protected Endpoints', function() {
 
   afterEach('cleanup', () => helpers.cleanTables(db));
 
-  beforeEach('insert things', () => {
-    return helpers.seedThingsTables(db, testUsers, testThings, testReviews);
-  });
+  beforeEach('insert things', () =>
+    helpers.seedThingsTables(db, testUsers, testThings, testReviews)
+  );
 
   const protectedEndpoints = [
     {
@@ -31,49 +32,78 @@ describe('Protected Endpoints', function() {
       path: '/api/things/1'
     },
     {
-      name: 'GET /api/things/:things_id',
+      name: 'GET /api/things/:things_id/reviews',
       path: '/api/things/1/reviews'
     }
   ];
 
   protectedEndpoints.forEach(endpoint => {
-    
     describe(endpoint.name, () => {
-      it('responds with 401 missing token when no basic token provided', () => {
+      it('responds with 401 missing token when no bearer token provided', () => {
         return supertest(app)
           .get(endpoint.path)
-          .expect(401, { error: 'Missing token' });
+          .expect(401, { error: 'Missing bearer token' });
       });
 
-      it('responds with 401 "Unauthorized request" when Credentials are missing', () => {
-        const missingCred = { user_name: '', password: '' };
+      it('responds with 401 "Unauthorized request" when JWT secret is invalid', () => {
+        const testUser = testUsers[0];
 
         return supertest(app)
           .get(endpoint.path)
-          .set('Authorization', helpers.makeAuthHeader(missingCred))
+          .set('Authorization', helpers.makeAuthHeader(testUser, 'fakesecret'))
           .expect(401, { error: 'Unauthorized request' });
       });
 
-      it("responds 401 if user isn't valid", () => {
-        const invalidUser = { user_name: 'NotAReal', password: 'password' };
+      it("responds 401 if subject isn't valid", () => {
+        const invalidUser = { user_name: 'NotAReal', id: 1 };
 
         return supertest(app)
           .get(endpoint.path)
-          .set('Authorization', helpers.makeAuthHeader(invalidUser))
+          .set(
+            'Authorization',
+            helpers.makeAuthHeader(invalidUser, config.JWT_SECRET)
+          )
           .expect(401, { error: 'Unauthorized request' });
       });
+    });
+  });
+  describe('POST /api/reviews', () => {
+    const endpoint = { path: '/api/reviews' };
+    const { testThings, testUsers } = helpers.makeThingsFixtures();
+    const newReview = {
+      text: 'Test new review',
+      rating: 3,
+      thing_id: testThings[0].id
+    };
 
-      it("responds 401 Unauthorized request if password doesn't match user_name", () => {
-        const invalidPassword = {
-          user_name: testUsers[0].user_name,
-          password: 'Notarealpassword'
-        };
+    it('responds with 401 missing token when no bearer token provided', () => {
+      return supertest(app)
+        .post(endpoint.path)
+        .send(newReview)
+        .expect(401, { error: 'Missing bearer token' });
+    });
 
-        return supertest(app)
-          .get(endpoint.path)
-          .set('Authorization', helpers.makeAuthHeader(invalidPassword))
-          .expect(401, { error: 'Unauthorized request' });
-      });
+    it('responds with 401 "Unauthorized request" when JWT secret is invalid', () => {
+      const testUser = testUsers[0];
+
+      return supertest(app)
+        .post(endpoint.path)
+        .send(newReview)
+        .set('Authorization', helpers.makeAuthHeader(testUser, 'fakesecret'))
+        .expect(401, { error: 'Unauthorized request' });
+    });
+
+    it("responds 401 if subject isn't valid", () => {
+      const invalidUser = { user_name: 'NotAReal', id: 1 };
+
+      return supertest(app)
+        .post(endpoint.path)
+        .send(newReview)
+        .set(
+          'Authorization',
+          helpers.makeAuthHeader(invalidUser, config.JWT_SECRET)
+        )
+        .expect(401, { error: 'Unauthorized request' });
     });
   });
 });
